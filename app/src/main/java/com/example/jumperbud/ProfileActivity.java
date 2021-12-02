80e5f4f86330a50bcb7d9b3c6349d4c3a813adce
Ugur Pekunsal <ugurpekunsal@gmail.com>
2021-12-02 13:54:06 -0500

    Logout feature added

--- app/src/main/java/com/example/jumperbud/ProfileActivity.java
+++ app/src/main/java/com/example/jumperbud/ProfileActivity.java
@@ -2,13 +2,31 @@ package com.example.jumperbud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ImageButton logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout = (ImageButton) findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, Login.class));
+            }
+        });
     }
 }
