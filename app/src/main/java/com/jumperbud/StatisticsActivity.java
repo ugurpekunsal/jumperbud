a3ee446f2b75e6c38c525a197bc063aee5bbe6eb
Ugur Pekunsal <ugurpekunsal@gmail.com>
2021-12-08 20:41:13 -0500

    Design of the statistics page is completed

--- app/src/main/java/com/jumperbud/StatisticsActivity.java
+++ app/src/main/java/com/jumperbud/StatisticsActivity.java
@@ -2,7 +2,17 @@ package com.jumperbud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class StatisticsActivity extends AppCompatActivity {
 
@@ -10,5 +20,31 @@ public class StatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        BarChart chart = (BarChart) findViewById(R.id.chart);
+        showBarChart(chart);
+    }
+
+    private void showBarChart(BarChart barChart){
+        ArrayList<Double> valueList = new ArrayList<Double>();
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        String title = "Title";
+
+        //input data
+        for(int i = 0; i < 6; i++){
+            valueList.add(i * 100.1);
+        }
+
+        //fit the data into a bar
+        for (int i = 0; i < valueList.size(); i++) {
+            BarEntry barEntry = new BarEntry(i, valueList.get(i).floatValue());
+            entries.add(barEntry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, title);
+
+        BarData data = new BarData(barDataSet);
+        barChart.setData(data);
+        barChart.invalidate();
     }
 }
